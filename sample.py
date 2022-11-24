a0d02ab5c21d577c56e8903fdac60bd06b1b5b90
mogan <mogunsundar@gmail.com>
2022-11-24 16:18:13 +0530

    my first commit

---
+++ sample.py
@@ -0,0 +1,17 @@
+mylist = [3 , 7 , 10 , 14 , 18 , 13]
+
+mylist_even=[]
+
+mylist_odd=[]
+
+for item in mylist:
+    if item % 2 == 0:
+        mylist_even.append(item)
+    else:
+        mylist_odd.append(item)
+
+print(mylist_even)
+
+print(mylist_odd)
+
+print("program successfully executed")
